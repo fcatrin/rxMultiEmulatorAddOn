ab1fd6674caab179520831853e7d5c941e6fc137
Franco Catrin <fcatrin@gmail.com>
2015-08-15 02:03:30 +0000

    Permite ejecutar juegos con RetroArch 1.2.2

--- android/phoenix/src/com/retroarch/browser/retroactivity/RetroBoxWrapper.java
+++ android/phoenix/src/com/retroarch/browser/retroactivity/RetroBoxWrapper.java
@@ -1,4 +1,5 @@
 package com.retroarch.browser.retroactivity;
+
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
@@ -12,49 +13,47 @@ import android.view.WindowManager;
 import com.retroarch.browser.preferences.util.UserPreferences;
 
 public class RetroBoxWrapper extends Activity {
-        private static final String RETROARCH_FUTURE = "retrobox.retroarch.future";
-        private static final String RETROARCH_PAST = "retrobox.retroarch.past";
-        
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        
-        Log.d("RetroArch", "RetroBoxWrapper");
-        
-                setTitle(getIntent().getStringExtra("TITLE"));
-                UserPreferences.updateConfigFile(this);
-                
-                Intent intent = new Intent();
-                intent.setAction(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB?RETROARCH_FUTURE:RETROARCH_PAST);
-                intent.putExtra("CONFIGFILE", UserPreferences.getDefaultConfigPath(this));
+	private static final String RETROARCH_FUTURE = "retrobox.retroarch.future";
+	private static final String RETROARCH_PAST = "retrobox.retroarch.past";
 
-                intent.fillIn(getIntent(), 0);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-                startActivityForResult(intent,1);
-                
-        }
+		Log.d("RetroArch", "RetroBoxWrapper");
 
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-                finish();
-        }
+		setTitle(getIntent().getStringExtra("TITLE"));
+		UserPreferences.updateConfigFile(this);
 
-        @TargetApi(Build.VERSION_CODES.KITKAT)
-        public static void setImmersiveMode(View decorView) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                        Log.d("RetroArch", "setImmersiveMode");
-                        decorView.setSystemUiVisibility(
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-                } else {
-                        
-                }
-        }
+		Intent intent = new Intent();
+		intent.setAction(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ? RETROARCH_FUTURE : RETROARCH_PAST);
+		intent.putExtra("CONFIGFILE", UserPreferences.getDefaultConfigPath(this));
+
+		intent.fillIn(getIntent(), 0);
+
+		startActivityForResult(intent, 1);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		finish();
+	}
+
+	@TargetApi(Build.VERSION_CODES.KITKAT)
+	public static void setImmersiveMode(View decorView) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+			Log.d("RetroArch", "setImmersiveMode");
+			decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+					| View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		} else {
+
+		}
+	}
 }
